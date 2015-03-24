615b65d32dbc03fa2ad4052c6e6c762315a90de8
Howon Lee <howon@howonlee.com>
2015-03-24 15:10:07 -0700

    duplication everywhere

---
+++ pgm.py
@@ -0,0 +1,39 @@
+import networkx as nx
+import random
+
+def wash_words(word_ls):
+    word_map = {}
+    curr_idx = 0
+    for word in word_ls:
+        if word not in word_map:
+            word_map[word] = curr_idx
+            curr_idx += 1
+    return [word_map[word] for word in word_ls], word_map
+
+def word_net(corpus_ls):
+    """
+    Takes a list of numbers
+    Do the number map if you want the words back
+    In this case, usually don't want the words back
+    """
+    bigrams = zip(corpus_ls, corpus_ls[1:])
+    net = nx.Graph()
+    net.add_edges_from(bigrams)
+    return net
+
+def select_net(net, p=0.9):
+    new_net = nx.Graph()
+    edge_list = net.edges()
+    new_net_edges = []
+    for edge in edge_list:
+        if random.random() < p:
+            new_net_edges.append(edge)
+    new_net.add_edges_from(new_net_edges)
+    return new_net
+
+if __name__ == "__main__":
+    with open("corpus.txt", "r") as corpus_file:
+        total_corpus = corpus_file.read().split()
+        washed, word_map = wash_words(total_corpus)
+        net = word_net(washed)
+        print net.number_of_edges()
